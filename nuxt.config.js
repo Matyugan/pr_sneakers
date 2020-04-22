55944a97d9e252c8a698736ab705a48600742f46
Максим Викторович <maxdev@multikey.studio>
2020-04-21 17:05:11 -0700

    Add favicon, Add meta-description

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Buy rare releases with honest lotteries' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
   css: [
